58c433d330c1de9ccf36e6442579fa11f0c729d1
Psychokiller1888 <laurentchervet@bluewin.ch>
2021-01-22 18:42:37 +0100

    Update version

--- RedQueen.py
+++ RedQueen.py
@@ -131,7 +131,7 @@ class RedQueen(AliceSkill):
 				chance = 25
 
 			if randint(0, 100) < chance:
-				self.say(text=self.randomTalk('thanksForBeingNice'), siteId=session.siteId)
+				self.say(text=self.randomTalk('thanksForBeingNice'), siteId=session.deviceUid)
 
 
 	def politnessUsed(self, text: str) -> bool:
@@ -174,19 +174,19 @@ class RedQueen(AliceSkill):
 
 	@IntentHandler('WhoAreYou')
 	def whoIntent(self, session: DialogSession):
-		self.endDialog(sessionId=session.sessionId, text=self.randomTalk('aliceInfos'), siteId=session.siteId)
+		self.endDialog(sessionId=session.sessionId, text=self.randomTalk('aliceInfos'), siteId=session.deviceUid)
 
 
 	@IntentHandler('GoodMorning')
 	def morningIntent(self, session: DialogSession):
 		self.SkillManager.skillBroadcast(constants.EVENT_WAKEUP)
 		time.sleep(0.5)
-		self.endDialog(sessionId=session.sessionId, text=self.randomTalk('goodMorning'), siteId=session.siteId)
+		self.endDialog(sessionId=session.sessionId, text=self.randomTalk('goodMorning'), siteId=session.deviceUid)
 
 
 	@IntentHandler('GoodNight')
 	def nightIntent(self, session: DialogSession):
-		self.endDialog(sessionId=session.sessionId, text=self.randomTalk('goodNight'), siteId=session.siteId)
+		self.endDialog(sessionId=session.sessionId, text=self.randomTalk('goodNight'), siteId=session.deviceUid)
 		self.SkillManager.skillBroadcast(constants.EVENT_SLEEP)
 
 
@@ -197,7 +197,7 @@ class RedQueen(AliceSkill):
 		if 'State' not in slots.keys():
 			self.logError('No state provided for changing user state')
 			self.endDialog(sessionId=session.sessionId, text=self.TalkManager.randomTalk('error', skill='system'),
-						   siteId=session.siteId)
+			               siteId=session.deviceUid)
 			return
 
 		# Todo re enable this if statement when the "personDesignation" code gets fully implemented
@@ -209,7 +209,7 @@ class RedQueen(AliceSkill):
 			self.logWarning(f"Unsupported user state \"{slots['State'][0].value['value']}\"")
 
 		self.endDialog(sessionId=session.sessionId, text=self.TalkManager.randomTalk(slots['State'][0].value['value']),
-					   siteId=session.siteId)
+		               siteId=session.deviceUid)
 
 
 	def randomlySpeak(self, init: bool = False):
